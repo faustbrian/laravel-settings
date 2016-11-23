71b1eb0b47411ccc2ce8c11aa6863533988f34c5
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 16:16:03 +0200

    Applied fixes from StyleCI (#3)
    [ci skip] [skip ci]

--- src/Facades/Setting.php
+++ src/Facades/Setting.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Settings.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Settings\Facades;
 
 use Illuminate\Support\Facades\Facade;
--- src/Store/DatabaseStore.php
+++ src/Store/DatabaseStore.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Settings.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Settings\Store;
 
 use Illuminate\Database\Connection;
@@ -146,7 +155,7 @@ class DatabaseStore extends Store
     {
         $query = $this->connection->table($this->table);
 
-        if (!$insert) {
+        if (! $insert) {
             foreach ($this->extraColumns as $key => $value) {
                 $query->where($key, '=', $value);
             }
